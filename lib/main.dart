e69a581b9e5e00e6c4a2c7f74744c839c83960a5
LucasHor4 <lucash.arcanjo267@gmail.com>
2024-11-11 17:09:51 -0300

    Criando arquivos (Telas).

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 10,
+                ),
+                child: const Icon(
+                  Icons.home,
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(
+                  
+
+                ),
+                
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/Pag2');
+                    },
+                    
+                    child: const Text('Animais'),
+                    ),
+              )
+            ],
+          ),
       ),
+      routes: {
+        //'/Pag2': (context) => const Pag2(),
+      },
     );
   }
 }
