9f3e9e4a56f06680e054a73108aae4df4387de96
LucasHor4 <lucash.arcanjo267@gmail.com>
2024-11-11 17:16:28 -0300

    Adicionando codigos "base" para telas.

---
+++ lib/animalTela.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(animalTela());
+}
+
+class animalTela extends StatelessWidget {
+  animalTela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 10,
+                ),
+                child: const Icon(
+                  Icons.home,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  
+
+                ),
+                
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/Pag2');
+                    },
+                    
+                    child: const Text('Animais'),
+                    ),
+              )
+            ],
+          ),
+      ),
+      routes: {
+        //'/Pag2': (context) => const Pag2(),
+      },
+    );
+  }
+}
---
+++ lib/habitatTela.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(habitatTela());
+}
+
+class habitatTela extends StatelessWidget {
+  habitatTela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 10,
+                ),
+                child: const Icon(
+                  Icons.home,
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(
+                  
+
+                ),
+                
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/Pag2');
+                    },
+                    
+                    child: const Text('Animais'),
+                    ),
+              )
+            ],
+          ),
+      ),
+      routes: {
+        //'/Pag2': (context) => const Pag2(),
+      },
+    );
+  }
+}
